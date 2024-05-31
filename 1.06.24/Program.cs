a6b9dfa9d707ad6aa77966be18405d18916f5951
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-05-31 22:49:35 +0300

    Add project files.

---
+++ 1.06.24/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1._06._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
